1e2f05689ab615df0e3f954faa43701468a0b5f1
mriyank <mriyanksingh@gmail.com>
2020-11-23 19:52:03 +0530

    added dropdown

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -22,23 +23,26 @@ function App() {
     getCountriesData();
   }, []);
 
+  const onCountryChange = (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>Covid-19 tracker</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
-
-            {/* <MenuItem vlaue="Worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">option2</MenuItem>
-            <MenuItem value="worldwide">option3</MenuItem>
-            <MenuItem value="worldwide">option4</MenuItem> */}
           </Select>
         </FormControl>
       </div>
+
+      <div className="app__stats"></div>
     </div>
   );
 }
