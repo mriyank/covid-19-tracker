c28036cf698ec52603e956bfde9826147fc29b1a
mriyank <mriyanksingh@gmail.com>
2020-11-25 22:59:16 +0530

    website ready

--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
 import React from "react";
+import numeral from "numeral";
 import { Circle, Popup } from "react-leaflet";
-import { numeral } from "numeral";
 
 const casesTypeColors = {
   cases: {
@@ -24,19 +24,19 @@ const casesTypeColors = {
 };
 
 export const sortData = (data) => {
-  const sortedData = [...data];
-
-  return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
-
-  // sortedData.sort((a, b) => {
-  //   if (a.cases > b.cases) {
-  //     return -1;
-  //   } else {
-  //     return 1;
-  //   }
-  // });
-  // return sortedData;
+  let sortedData = [...data];
+  sortedData.sort((a, b) => {
+    if (a.cases > b.cases) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  return sortedData;
 };
+
+export const prettyPrintStat = (stat) =>
+  stat ? `+${numeral(stat).format("0.0a")}` : "+0";
 
 export const showDataOnMap = (data, casesType = "cases") =>
   data.map((country) => (
@@ -44,9 +44,28 @@ export const showDataOnMap = (data, casesType = "cases") =>
       center={[country.countryInfo.lat, country.countryInfo.long]}
       color={casesTypeColors[casesType].hex}
       fillColor={casesTypeColors[casesType].hex}
-      fillopacity={0.4}
+      fillOpacity={0.4}
       radius={
         Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
       }
-    />
+    >
+      <Popup>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          ></div>
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
+      </Popup>
+    </Circle>
   ));
